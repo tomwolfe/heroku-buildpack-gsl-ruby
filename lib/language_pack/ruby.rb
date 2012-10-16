a4ee693d9ffa76795db38587c32160e74d1ba774
Thomas Wolfe <tomwolfe@gmail.com>
2012-10-15 22:20:52 -0500

    Update lib/language_pack/ruby.rb

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -59,7 +59,7 @@ class LanguagePack::Ruby < LanguagePack::Base
       run("cp -R vendor/gsl /app/vendor/gsl")
       # headers for gsl (used for rb-gsl/gsl gem)
       run("mkdir -p /usr/include/gsl")
-      run("cp -R vendor/gsl/include /usr/include/gsl")
+      run("cp -R vendor/gsl/include/gsl /usr/include/gsl")
       install_language_pack_gems
       build_bundler
       create_database_yml
